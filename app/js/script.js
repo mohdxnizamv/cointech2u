9177a95013a6a2d80f1abb339134c60d29b5376d
Mohd Nizamuddin Mad Yusof <58153279+mohdxnizamv@users.noreply.github.com>
2023-06-20 11:34:16 +0800

    commit

--- app/js/script.js
+++ app/js/script.js
@@ -7,9 +7,6 @@ const logo = document.getElementById("logo")
 // Tabs menu event listener
 tabs.forEach((tab) => tab.addEventListener("click", onTabClick))
 
-// Hamburger button listener
-btn.addEventListener("click", navToggle)
-
 function onTabClick(e) {
   // Deactivate all tabs
   tabs.forEach((tab) => {
@@ -30,16 +27,4 @@ function onTabClick(e) {
     .getElementById("panels")
     .getElementsByClassName(classString)[0]
     .classList.remove("hidden")
-}
-
-function navToggle() {
-  btn.classList.toggle("open")
-  menu.classList.toggle("flex")
-  menu.classList.toggle("hidden")
-
-  if (menu.classList.contains("flex")) {
-    logo.setAttribute("src", "./app/images/logo_min.png")
-  } else {
-    logo.setAttribute("src", "./app/images/logo_min.png")
-  }
 }
